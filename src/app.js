76d61a1ceb7f9b084b258318e1528363273ee50e
jqim87 <jqim87@gmail.com>
2019-07-17 11:23:02 +0100

    format

--- src/app.js
+++ src/app.js
@@ -3,12 +3,12 @@ import Vue from "vue";
 import machines from "./data/machines.json";
 
 var box = new Vue({
-    el: "#container",
-    data: {
-        title: "Title from Vue data!!"
-    }
+  el: "#container",
+  data: {
+    title: "Title from Vue data!!"
+  }
+});
 
-})
 Object.keys(machines).forEach(key => {
-    console.log(machines[key]);
-  });
+  console.log(machines[key]);
+});
